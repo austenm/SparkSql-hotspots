b49384fba4cfea953af1112f57db785f8bc1e87d
Ashis Philip <ashisphilip@Ashiss-MacBook-Pro.local>
2020-11-13 18:27:54 -0700

    Review comments incorporated

--- src/main/scala/shapes/Point.scala
+++ src/main/scala/shapes/Point.scala
@@ -1,22 +1,32 @@
 package shapes
 
-class Point (var x: Double, var y: Double) {
-  // This is the point class
-  var xc: Double = x
-  var yc: Double = y
+class Point  (val x: Double, val y: Double) {
 
 }
+/**
+ * Companion object used for utility functions
+ */
 object Point {
+  /**
+   * Converts to a point
+   * @param pointsString co-ordinate string
+   * @return a object of point with x and y coordinates
+   *
+   * @throws IllegalArgumentException
+   *  - does not contain two values separated by the regex
+   */
   def fromString(pointsString: String): Point = {
     val pointsArr = pointsString.split(",")
+    if (pointsArr.length != 2) {
+      throw new IllegalArgumentException("Points string must contain two coordinates")
+    }
     val xval = pointsArr(0).trim().toDouble
     val yval = pointsArr(1).trim().toDouble
-    val pt = new Point(xval, yval)
-    return pt
+    new Point(xval, yval)
+
   }
   def apply(x: Double, y: Double): Point = {
-     val pt = new Point(x,y)
-     return pt
+     new Point(x,y)
   }
 }
 
--- src/main/scala/shapes/Rectangle.scala
+++ src/main/scala/shapes/Rectangle.scala
@@ -1,55 +1,39 @@
 package shapes
 
 
-class Rectangle (var p1: Point, var p2: Point)  {
+class Rectangle private (val minpoint: Point, val maxpoint: Point)  {
 
   def contains(point: Point): Boolean = {
-    if (point.x >= p1.x && point.x <= p2.x && point.y >= p1.y && point.y <= p2.y) {
-      return true
-    } else {
-      return false
-    }
+   point.x >= minpoint.x && point.x <= maxpoint.x && point.y >= minpoint.y && point.y <= maxpoint.y
   }
 
 }
-object Rectangle {
 
+/**
+ * Companion object used for utility functions
+ */
+object Rectangle {
+  /**
+   * Creates an object rectangle with min and max coordinates
+   * @param rectangleString
+   * @return Rectangle object
+   * @throws IllegalArgumentException
+   *  - Need to contain the four coordinates of a rectangle
+   */
   def fromString(rectangleString: String): Rectangle = {
       val rectangleStrArr = rectangleString.split(",")
-      val rectanglex1 = rectangleStrArr(0).trim().toDouble
-      val rectangley1 = rectangleStrArr(1).trim().toDouble
-      val rectanglex2 = rectangleStrArr(2).trim().toDouble
-      val rectangley2 = rectangleStrArr(3).trim().toDouble
-      var p1c: Point = new Point(rectanglex1, rectangley1)
-      var p2c: Point = new Point(rectanglex2, rectangley2)
-      // This is the Rectangle class
-      var min_x: Double = 0
-      var max_x: Double = 0
-      var min_y: Double = 0
-      var max_y: Double = 0
-      if (p1c.x < p2c.x) {
-      min_x = p1c.x
-      max_x = p2c.x
-    } else {
-      min_x = p2c.x
-      max_x = p1c.x
-    }
-      if (p1c.y < p2c.y) {
-      min_y = p1c.y
-      max_y = p2c.y
-    } else {
-      min_y = p2c.y
-      max_y = p1c.y
-    }
-      val minpoint = new Point(min_x, min_y)
-      val maxpoint = new Point(max_x, max_y)
-      val rectangleObj = new Rectangle(minpoint, maxpoint)
-      return rectangleObj
+      if (rectangleStrArr.length != 4) {
+      throw new IllegalArgumentException("Rectangle string must contains four points")
+      }
+      val xpoints = List(rectangleStrArr(0).trim().toDouble, rectangleStrArr(2).trim().toDouble).sorted
+      val ypoints = List(rectangleStrArr(1).trim().toDouble, rectangleStrArr(3).trim().toDouble).sorted
+      val minpoint = new Point(xpoints(0), ypoints(0))
+      val maxpoint = new Point(xpoints(1), ypoints(1))
+      apply(minpoint, maxpoint)
   }
 
 
   def apply(x: Point, y: Point): Rectangle = {
-    val rectangle1 = new Rectangle(x,y)
-    return rectangle1
+    new Rectangle(x,y)
   }
 }
